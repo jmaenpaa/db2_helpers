c15119a1385bd354e56087917b2f2d5c9fada17a
John Maenpaa <JohnMaenpaa@db2Solutions.com>
2020-12-08 15:59:57 -0600

    Fix directory structure for command files.

---
+++ src/db_commands/__init__.py
@@ -0,0 +1,5 @@
+#  Copyright (c) 2020. db2solutions LLC
+
+from db_commands.db_import_export import db_export
+from db_commands.db_import_export import db_import
+from db_commands.db_commands import db_credentials
