679c39feb40ca34d30b1e1e21074a08766209ef9
John Maenpaa <JohnMaenpaa@db2Solutions.com>
2020-12-10 13:28:30 -0600

    Increment fix level.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='db2_helpers',
-    version='0.1.2',
+    version='0.1.3',
     description='Helper functions for managing database connections',
     long_description=long_description,
     long_description_content_type='text/markdown',
